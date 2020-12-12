92fd48e918b9a4ab55f6f4b6ab3261ab9d528a47
Rodrigo Alonso <rodrigo.alonso.o@gmail.com>
2020-12-12 13:54:47 -0300

    lokomon: validators

---
+++ lokomon/attic/validators/st2.cpp
@@ -0,0 +1,24 @@
+#include "testlib.h"
+#include <vector>
+
+using namespace std;
+
+int main() {
+    registerValidation();
+    
+    int n = inf.readInt(2, 1_000_000, "n");
+    vector<bool> positions(N, false);
+    inf.readEoln();
+
+    for (int h = n - 1; h >= 0; h--) {
+        int position = inf.readInt(0, n - 1, "i");
+
+        ensuref(!positions[position], "Montaña de altura %d en posición %d ya ocupada.", h, position);
+        positions[position] = true;
+
+        if (h) inf.readSpace();
+        else inf.readEoln();
+    }
+
+    inf.readEof();
+}
